01a96059c58b6846a5603289641c4b3a54a1dab0
Mike Fisher <mike@mbfisher.com>
2018-02-07 22:30:52 +1000

    Shitty content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,56 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import demoGif from '../demo.gif'
+import Feature from '../components/Feature';
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="hero">
+      <div className="title">
+        <h1>packary</h1>
+        <h3>Affordable private package repositories</h3>
+      </div>
+      <div className="demo">
+        <img src={demoGif}/>
+      </div>
+    </div>
+    <div className="features">
+      <div className="feature">
+        <Feature title="Simple" icon="thing">
+          <p>Create a repository in one click, configure your package manager and push your code.
+            No hosting, proxying or configuration.
+          </p>
+        </Feature>
+      </div>
+      <div className="feature">
+        <Feature title="Reliable" icon="thing">
+          <p>Create a repository in one click, configure your package manager and push your code.
+            No hosting, proxying or configuration.
+          </p>
+        </Feature>
+      </div>
+      <div className="feature">
+        <Feature title="Made for Teams" icon="thing">
+          <p>Create a repository in one click, configure your package manager and push your code.
+            No hosting, proxying or configuration.
+          </p>
+        </Feature>
+      </div>
+    </div>
+    <div className="signup">
+      <div>
+        <h1>Sign up our beta</h1>
+        <p>We're looking for people to help us test!</p>
+      </div>
+      <form>
+        <div>
+          <input name="email" type="email" placeholder="Your email address"/>
+        </div>
+        <div>
+          <button type="submit">Sign up</button>
+        </div>
+      </form>
+    </div>
   </div>
 )
 
