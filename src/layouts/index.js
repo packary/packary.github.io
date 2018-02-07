6d6fe9c9e5cf5dcc6c3dc7b5cd8ce477184ddf1f
Mike Fisher <mike@mbfisher.com>
2018-02-07 12:15:56 +1000

    gatsby new & typography

---
+++ src/layouts/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import T from 'prop-types'
+import Helmet from 'react-helmet'
+
+import './index.css'
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Gatsby Default Starter"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    {children()}
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: T.func,
+}
+
+export default TemplateWrapper
