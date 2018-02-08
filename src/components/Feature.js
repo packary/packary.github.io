899ba932643664345d2534bebd8560d0b37f2eac
Mike Fisher <mike@mbfisher.com>
2018-02-08 12:39:13 +1000

    First commit for gh-pages

---
+++ src/components/Feature.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Feature = ({ title, icon, children }) => (
+  <div>
+    <div>{icon}</div>
+    <h4>{title}</h4>
+    <p>{children}</p>
+  </div>
+)
+
+export default Feature
