6fb6a3222cf1d1845fcff190717934c00f09787b
Mike Fisher <mike@mbfisher.com>
2018-02-13 23:33:58 +1100

    Dans tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,56 +14,60 @@ const IndexPage = () => (
         <img src={demoGif} />
       </div>
     </div>
-    <div className="features">
-      <div className="feature">
-        <Feature title="Easy" icon="lightbulb">
-          <p>
-            Use our simple UI to deploy a registry in one click, and copy-paste your package manager configs.
-            No hosting, proxying or configuration.
-          </p>
-        </Feature>
-      </div>
-      <div className="feature">
-        <Feature title="Versatile" icon="users">
-          <p>
-            Keep your Maven artefacts, Docker containers, NPM and PyPi packages in one place.
-          </p>
-        </Feature>
-      </div>
-      <div className="feature">
-        <Feature title="Secure & Reliable" icon="lock">
-          <p>
-            Everything is over HTTPS and safe from prying eyes.
-            We store your code on multi-region cloud providers, so it's always available.
-          </p>
-        </Feature>
-      </div>
-      <div className="feature">
-        <Feature title="Made for Teams" icon="users">
-          <p>
-            Each developer has an account, and you control who can pull this and publish that.
-          </p>
-        </Feature>
+    <div className="container">
+      <div className="features">
+        <div className="feature">
+          <Feature title="Easy" icon="lightbulb">
+            <p>
+              Use our simple UI to deploy a registry in one click, and copy-paste your package manager configs.
+              No hosting, proxying or configuration.
+            </p>
+          </Feature>
+        </div>
+        <div className="feature">
+          <Feature title="Versatile" icon="users">
+            <p>
+              Keep your Maven artefacts, Docker containers, NPM and PyPi packages in one place.
+            </p>
+          </Feature>
+        </div>
+        <div className="feature">
+          <Feature title="Secure & Reliable" icon="lock">
+            <p>
+              Everything is over HTTPS and safe from prying eyes.
+              We store your code on multi-region cloud providers, so it's always available.
+            </p>
+          </Feature>
+        </div>
+        <div className="feature">
+          <Feature title="Made for Teams" icon="users">
+            <p>
+              Each developer has an account, and you control who can pull this and publish that.
+            </p>
+          </Feature>
+        </div>
       </div>
     </div>
     <div className="signup">
-      <div>
-        <h2>Sign up for our beta</h2>
-        <p>We nearly have Maven repositories ready to test; sign up for early access and we'll invite
-          you soon!</p>
-        <p className="spam">We won't send you anything other than your invitation unless you opt in.</p>
-      </div>
-      <form>
-        <p>
-          <div className="icon-input">
-            <i className="icon-mail"/>
-            <input name="email" type="email" placeholder="Email address" />
+      <div className="container">
+        <div>
+          <h2>Sign up for our beta</h2>
+          <p>We nearly have Maven repositories ready to test; sign up for early access and we'll invite
+            you soon!</p>
+          <p className="spam">We won't send you anything other than your invitation unless you opt in.</p>
+        </div>
+        <form>
+          <div className="input">
+            <div className="icon-input">
+              <i className="icon-mail"/>
+              <input name="email" type="email" placeholder="Email address" />
+            </div>
           </div>
-        </p>
-        <p>
-          <button type="submit">Sign up</button>
-        </p>
-      </form>
+          <div className="input">
+            <button type="submit">Sign up</button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 )
