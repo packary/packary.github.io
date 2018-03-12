01795aebeddcd75a62c4b29e04921402082ed31f
dan-j <5727701+dan-j@users.noreply.github.com>
2018-03-12 12:08:18 +1100

    formatting things

--- src/components/Home/FurtherDetails.js
+++ src/components/Home/FurtherDetails.js
@@ -4,14 +4,14 @@ import './styles/further-details.scss';
 const FurtherDetails = () => (
   <div className="container further-details">
     <div className="further-details__block">
-      <h1>Where we're up to</h1>
-      <p>We're on the verge of opening an invitation-based beta with support for Maven repositories, after that we're
-        looking to involve the community on what to deliver next! The platform has been built with other languages in
-        mind, so anything's possible!</p>
+      <h1 className="further-details__title">Where we're up to</h1>
+      <p className="further-details__p">We're on the verge of opening an invitation-based beta with support for Maven
+        repositories, after that we're looking to involve the community on what to deliver next! The platform has been
+        built with other languages in mind, so anything's possible!</p>
     </div>
     <div className="further-details__block">
       <p>A high-level road map includes (in no particular order)</p>
-      <ul>
+      <ul className="further-details__list">
         <li>Maven snapshots</li>
         <li>NPM support</li>
         <li>Python support</li>
--- src/components/Home/SuperSimple.js
+++ src/components/Home/SuperSimple.js
@@ -1,30 +1,26 @@
 import React from 'react';
 import './styles/be-in-control.scss';
-import cogIcon from '../../svgs/cog-outline-grad.svg';
-import stackIcon from '../../svgs/stack-grad.svg';
-import teamIcon from '../../svgs/team-grad.svg';
+import oneIcon from '../../svgs/one.svg';
+import twoIcon from '../../svgs/two.svg';
+import threeIcon from '../../svgs/three.svg';
 
 const SuperSimple = () => (
   <div className="container in-control">
-    <h1 className="center">Super simple hosting for your private packages</h1>
-    <p className="center smaller light">Most organisations have private libraries that need hosting + distributing,
-      but many alternatives are costly or require maintenance</p>
     <div className="in-control__info">
       <div className="in-control__info__item">
-        <img className="in-control__info__item__icon" src={cogIcon} />
-        <h3>Simple</h3>
-        <p>Create a repository in a few clicks, configure your package manager, and push your code. No hosting,
-          proxy'ing and minimal configuration.</p>
+        <img className="in-control__info__item__icon" src={oneIcon} />
+        <p>Choose your language and deploy a registry in
+          one-click. Your first registry is free.</p>
       </div>
       <div className="in-control__info__item">
-        <img className="in-control__info__item__icon" src={stackIcon} />
-        <h3>Reliable</h3>
-        <p>We store your code on multi-region cloud providers, so it's always available.</p>
+        <img className="in-control__info__item__icon" src={twoIcon} />
+        <p>Use our simple docs to configure your package manager. Chunky config blocks are ready to
+          copy-paste.</p>
       </div>
       <div className="in-control__info__item">
-        <img className="in-control__info__item__icon" src={teamIcon} />
-        <h3>Made for teams</h3>
-        <p>Each developer gets their own account, and you control who can publish what.</p>
+        <img className="in-control__info__item__icon" src={threeIcon} />
+        <p>Our registries use the same APIs as their open source counterparts, so they'll work with
+          all your tooling.</p>
       </div>
     </div>
   </div>
